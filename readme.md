546117b7ee388dafd3a685c6c4fcef944d0f416d
dragonblitz10 <developer@dragondev.party>
2021-05-28 07:15:31 +0100

    Refactor: General rewrite of endpoints, better error handling and proper returns

--- readme.md
+++ readme.md
@@ -1,32 +1,40 @@
 # XboxAPI
+
 An API wrapper for Xbox API.
 [![NPM](https://nodei.co/npm/@shadow/xbox.png)](https://nodei.co/npm/@shadow/xbox/)
 
 # Installing
+
 `npm i @shadow/xbox`
 
 # Usage
+
 ```js
-const XboxClient = require('@shadow/xbox');
-const xbox = new XboxClient('Your API Key Goes Here');
+const XboxClient = require("@shadow/xbox");
+const xbox = new XboxClient("Your API Key Goes Here");
 ```
 
 # Example
+
 ```js
-const XboxClient = require('@shadow/xbox');
-const xbox = new XboxClient('Your API Key Goes Here');
+const XboxClient = require("@shadow/xbox");
+const xbox = new XboxClient("Your API Key Goes Here");
 async function find(query) {
-let req = await xbox.id(query)
-return console.log(req)
+  const req = await xbox.id(query);
+  console.log(req)
+  return req;
 }
-find('2535412670012736')
+find("2535412670012736");
 ```
+
 # Getting API Key
+
 Go to [xapi.us](https://xapi.us/) and sign up for API key.
 
 # Functions
+
 - id
-- selfID
+- self
 - messages
 - conversations
 - gamertag
@@ -37,29 +45,34 @@ Go to [xapi.us](https://xapi.us/) and sign up for API key.
 - activity_recent
 - friends
 - followers
-- recentplayers
-- friendsplaying
+- recent_players
+- friends_playing
 - gameclips
 - screenshots
-- activityfeed
-- addfriend
-- removefriend
-- addfavourite
+- activity_feed
+- add_friend
+- remove_friend
+- add_favourite
 
 # Notes
-gameclips(id, true) will return saved clips
-friendsPlaying(id, gameTitle ID) 
-activity(ID, true) will return recent 
 
-# RateLimits 
-![Ratelimit](https://i.imgur.com/8JJEwlC.png)
+gameclips(id, true) will return saved clips
+friends_playing(id, gameTitle ID)
+activity(ID, true) will return recent
+
+# RateLimits
+
+![Ratelimit](https://i.imgur.com/mu5G5rK.png)
 
 API requests are limited on a per API Key basis.
-When making a request to the API, your limit information will be returned in the headers. 
+When making a request to the API, your limit information will be returned in the headers.
 
 # Ratelimit Headers:
+- I do not include ratelimits in the responses.
 - X-RateLimit-Limit: Maximum usage allowed per timeframe
 - X-Ratelimit-Remaining: Remaining requests that can be made during the timeframe
 - X-RateLimit-Reset: Timestamp indicating when the ratelimit will reset in milliseconds.
+
 # API Docs
+
 **[Click Here](https://xapi.us/documentation)**
--- src/test.js
+++ src/test.js
@@ -1,34 +1,106 @@
-const  { XboxClient } = require('./index')
-const Xbox = new XboxClient('ID')
+const { XboxClient } = require('./index');
+const xbox = new XboxClient('Token');
+const user = 'djekl';
+const id = '2533274813081462';
 
-async function test() {
-// let testID = await Xbox.id('steve') 
-// let selfID = await Xbox.selfID()
-// let messages = await Xbox.messages()
-// let conversations = await Xbox.conversations()
-// let gamertag = await Xbox.gamertag('2535412670012736')
-// let profile = await Xbox.profile()
-// let New_Profile = await Xbox.profile('2535412670012736')
-// let GamerCard = await Xbox.profile('2535412670012736')
-// let presence = await Xbox.presence('2535412670012736')
-
-// let activity = await Xbox.activity('2535412670012736')
-// let activity = await Xbox.activity('2535412670012736', true)
-// setting to true will get recent activity 
-
-// let friends = await Xbox.friends('2535412670012736')
-// let followers = await Xbox.followers('2535412670012736')
-// let recentPlayers = await Xbox.recentPlayers()
-// let friendsPlaying = await Xbox.friendsPlaying('2535412670012736', '950328474')
-
-// let gameclips = await Xbox.gameclips('2535441526102096')
-// let gameclips = await Xbox.gameclips('2535441526102096', true) 
-// setting true for saved
-
-// let screenshots = await Xbox.screenshots('2535441526102096')
-// let activityfeed  = await Xbox.activityfeed()
-// let addfriend = await Xbox.addfriend('2535407978159585')
-// let removefriend = await Xbox.addfriend('2535407978159585')
-// let addfavourite = await Xbox.addfavourite('2535407978159585')
+async function self() {
+	const self = await xbox.self();
+	return self;
 }
-test()
+async function _id() {
+	const _id = await xbox.id(user);
+	console.log(_id);
+	return _id;
+}
+async function messages() {
+	const messages = await xbox.messages();
+	console.log(messages);
+	return messages;
+}
+async function conversations() {
+	const conversations = await xbox.conversations();
+	console.log(conversations);
+	return conversations;
+}
+async function gamertag() {
+	const gamertag = await xbox.gamertag(id);
+	console.log(gamertag);
+	return gamertag;
+}
+async function self_profile() {
+	const self_profile = await xbox.self_profile(id);
+	console.log(self_profile);
+	return self_profile;
+}
+async function profile() {
+	const profile = await xbox.profile(id);
+	console.log(profile);
+	return profile;
+}
+async function presence() {
+	const presence = await xbox.presence(id);
+	console.log(presence);
+	return presence;
+}
+async function activity() {
+	const activity = await xbox.activity(id);
+	console.log(activity);
+	return activity;
+}
+async function activity_recent(id) {
+	const activity_recent = await xbox.activity_recent(id);
+	console.log(activity_recent);
+	return activity_recent;
+}
+async function friends() {
+	const friends = await xbox.friends(id);
+	console.log(friends);
+	return friends;
+}
+async function followers() {
+	const followers = await xbox.followers(id);
+	console.log(followers);
+	return followers;
+}
+async function recent_players() {
+	const recent_players = await xbox.recent_players(id);
+	console.log(recent_players);
+	return recent_players;
+}
+async function friends_playing() {
+	const friends_playing = await xbox.friends_playing(id, '950328474');
+	console.log(friends_playing);
+	return friends_playing;
+}
+async function gameclips() {
+	const gameclips = await xbox.gameclips(id);
+	console.log(gameclips);
+	return gameclips;
+}
+async function gameclips_saved() {
+	const gameclips_saved = await xbox.gameclips_saved(id);
+	console.log(gameclips_saved);
+	return gameclips_saved;
+}
+async function screenshots() {
+	const screenshots = await xbox.screenshots;
+	console.log(screenshots);
+	return screenshots;
+}
+async function activity_feed() {
+	const activity_feed = await xbox.activity_feed();
+	console.log(activity_feed);
+	return activity_feed;
+}
+async function add_friend() {
+	const add_friend = await xbox.add_friend(id);
+	return add_friend;
+}
+async function add_favourite() {
+	const add_favourite = await xbox.add_favourite(id);
+	return add_favourite;
+}
+async function remove_friend() {
+	const remove_friend = await xbox.remove_friend(id);
+	return remove_friend;
+}
